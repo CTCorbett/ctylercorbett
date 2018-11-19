52cacf91c5ecff5b500e02ea5a782a0717245aea
Tyler Corbett <tcorbett@unum.com>
2018-11-18 20:59:28 -0500

    set up sass automation

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,32 @@
 // Sass configuration
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
+var reload = browserSync.reload;
 
-gulp.task('sass', function() {
-    gulp.src('scripts/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest(function(f) {
-            return f.base;
-        }))
+var src = {
+    scss: 'styles/*.scss',
+    css: 'styles/',
+    html: '*.html'
+};
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: './'
+    });
+
+    gulp.watch(src.scss, ['sass']);
+    gulp.watch(src.html).on('change', reload);
 });
 
-gulp.task('default', ['sass'], function() {
-    gulp.watch('scripts/*.scss', ['sass']);
-})
+// Compile sass into CSS
+gulp.task('sass', function() {
+    return gulp
+        .src(src.scss)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(src.css))
+        .pipe(reload({ stream: true }));
+});
+
+gulp.task('default', ['serve']);
