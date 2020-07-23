e1cfb0f0e35c482640d94753c50053aa757c0fc6
Tyler Corbett <ctylercorbett@gmail.com>
2020-07-23 16:58:09 -0400

    modal bugfixes
    fixed flexbox alignment cause by new #wrapper container
    added modal animation
    replaced glyph icon
    misc css tweaks
    butchered js attempt to deactivate modal
    removed unused commented code

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,10 +1,26 @@
 $(document).ready(function() {
   // Triutum sigil is clicked
   $("#triutum").click(function() {
-
-  // Modal state changes
-  $("body").toggleClass("dialogIsOpen");
-  console.log("logged");
-
+    // Modal state changes
+    $("body").toggleClass("dialogIsOpen");
   });
+
+  // Deactivate on ESC
+  function onDocumentKeyUp( event ) {
+    if( event.keyCode === 27 ) {
+      if($("body").hasClass("dialogIsOpen")) {
+        $("body").toggleClass("dialogIsOpen");
+      }
+    }
+  }
+
+  // Deactivate on click
+  function onDocumentClick( event ) {
+    if( event.target === cover ) {
+      if($("body").hasClass("dialogIsOpen")) {
+        $("body").toggleClass("dialogIsOpen");
+      }
+    }
+  }
+
 });
