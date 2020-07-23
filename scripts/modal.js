e1cf63dc707e9e880de110d2539d7aad0ab2a3ee
Tyler Corbett <ctylercorbett@gmail.com>
2020-07-23 01:10:58 -0400

    scaffolded modal
    added content, styling, and initializing behavior for a supplemental model window.
    
    broke the footer's flex positioning

---
+++ scripts/modal.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  // Triutum sigil is clicked
+  $("#triutum").click(function() {
+
+  // Modal state changes
+  $("body").toggleClass("dialogIsOpen");
+  console.log("logged");
+
+  });
+});
